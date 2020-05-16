09de54ce158ded93a6a91b23787be6b501f16e60
Dien Tran <usbv135w@gmail.com>
2020-05-16 01:08:00 -0500

    Ignore temp files, added phase 1 handling

--- cmd/main.go
+++ cmd/main.go
@@ -17,7 +17,31 @@ import (
 	. "github.com/dienakakim/r6mapbanbackend/includes"
 )
 
-const SESSIONS_FILENAME = "sessions.gob"
+const DATA_FILENAME = "data.gob"
+
+// Allowed maps
+const (
+	BANK              = "Bank"
+	BORDER            = "Border"
+	CHALET            = "Chalet"
+	CLUBHOUSE         = "Clubhouse"
+	COASTLINE         = "Coastline"
+	CONSULATE         = "Consulate"
+	KAFE              = "Kafe"
+	KANAL             = "Kanal"
+	OREGON            = "Oregon"
+	OUTBACK           = "Outback"
+	THEMEPARK         = "Theme Park"
+	VILLA             = "Villa"
+	FAVELA            = "Favela"
+	FORTRESS          = "Fortress"
+	HEREFORDBASE      = "Hereford Base"
+	HOUSE             = "House"
+	PRESIDENTIALPLANE = "Presidential Plane"
+	SKYSCRAPER        = "Skyscraper"
+	TOWER             = "Tower"
+	YACHT             = "Yacht"
+)
 
 func main() {
 	port := flag.Int("port", 4000, "port to serve")
@@ -25,22 +49,34 @@ func main() {
 
 	// Session map
 	var sessionMap map[string]Session
+	var mapPool map[string]bool
 
-	// Check if SESSIONS_FILENAME exists
-	sessionsGob, err := os.Open(SESSIONS_FILENAME)
+	// Check if DATA_FILENAME exists
+	dataGob, err := os.Open(DATA_FILENAME)
 	if err != nil {
 		if os.IsNotExist(err) {
-			// No such file found, create new map
-			sessionMap = make(map[string]Session)
+			// No such file found, create new session map and map pool
+			log.Printf("\"%s\" not found; created new", DATA_FILENAME)
+			maps := []string{BANK, BORDER, CHALET, CLUBHOUSE, COASTLINE, CONSULATE, FAVELA, FORTRESS, HEREFORDBASE, HOUSE, KAFE, KANAL, OREGON, OUTBACK, PRESIDENTIALPLANE, SKYSCRAPER, THEMEPARK, TOWER, VILLA, YACHT}
+			for _, m := range maps {
+				mapPool[m] = true
+			}
 		} else {
 			// File is found, but cannot be opened
-			log.Fatalf("File \"%s\" cannot be opened", SESSIONS_FILENAME)
+			log.Fatalf("File \"%s\" cannot be opened", DATA_FILENAME)
 		}
 	} else {
 		// File opened, decode gob into sessionMap
-		sessionsGobDecoder := gob.NewDecoder(sessionsGob)
-		sessionsGobDecoder.Decode(&sessionMap)
-		sessionsGob.Close()
+		dataGobDecoder := gob.NewDecoder(dataGob)
+		err := dataGobDecoder.Decode(&sessionMap)
+		if err != nil {
+			log.Println("Session map cannot be decoded")
+		}
+		err = dataGobDecoder.Decode(&mapPool)
+		if err != nil {
+			log.Println("Map pool cannot be decoded")
+		}
+		dataGob.Close()
 	}
 
 	// Set up exit handler
@@ -54,15 +90,16 @@ func main() {
 			case syscall.SIGINT:
 				// User requested termination
 				log.Printf("%v received", sig)
-				// Save session map into SESSIONS_FILENAME
-				file, err := os.Create(SESSIONS_FILENAME)
+				// Save session map and map pool into DATA_FILENAME
+				file, err := os.Create(DATA_FILENAME)
 				defer file.Close()
 				if err != nil {
 					log.Panic("Sessions cannot be marshaled!!!")
 				}
-				sessionsGobEncoder := gob.NewEncoder(file)
-				sessionsGobEncoder.Encode(sessionMap)
-				// Session map saved, can now exit
+				dataGobEncoder := gob.NewEncoder(file)
+				dataGobEncoder.Encode(sessionMap)
+				dataGobEncoder.Encode(mapPool)
+				// Data saved, can now exit
 				done <- true
 				log.Printf("Sessions marshaled")
 			}
@@ -72,7 +109,7 @@ func main() {
 	// Associate handlers
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", http.NotFound)
-	mux.HandleFunc("/process", handlerBuilder(sessionMap))
+	mux.HandleFunc("/process", handlerBuilder(sessionMap, mapPool))
 
 	log.Printf("Starting server on port %d", *port)
 	go func() {
@@ -81,7 +118,7 @@ func main() {
 			log.Fatal("Server cannot be started, terminating")
 		}
 	}()
-	log.Printf("Waiting on signal")
+	log.Printf("Server started. Send SIGINT to exit")
 	<-done
 	log.Printf("Done.")
 }
@@ -96,7 +133,7 @@ func generateToken() string {
 	return base64.URLEncoding.Strict().EncodeToString(b[:])
 }
 
-func handlerBuilder(sessionMap map[string]Session) func(http.ResponseWriter, *http.Request) {
+func handlerBuilder(sessionMap map[string]Session, mapPool map[string]bool) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", http.MethodPost)
@@ -123,31 +160,58 @@ func handlerBuilder(sessionMap map[string]Session) func(http.ResponseWriter, *ht
 			//		orangeTeamName: "...",
 			//		blueTeamName: "..."
 			// }
-			var t TeamNames
+			var initSession InitSession
 			b := make([]byte, r.ContentLength)
 			r.Body.Read(b)
-			if err := json.Unmarshal(b, &t); err != nil {
+			if err := json.Unmarshal(b, &initSession); err != nil {
 				log.Println("Cannot decode json")
 				w.WriteHeader(http.StatusBadRequest)
 				fmt.Fprintln(w, "Malformed JSON body")
 				return
 			}
 
-			// Check for orangeTeamName and blueTeamName
-			if t.BlueTeamName == nil || t.OrangeTeamName == nil {
-				log.Println("Team names missing")
+			// Check for nil fields
+			if initSession.BlueTeamName == nil || initSession.OrangeTeamName == nil || initSession.MapPool == nil {
 				w.WriteHeader(http.StatusBadRequest)
-				fmt.Fprintln(w, "Required team names missing")
+				if initSession.OrangeTeamName == nil {
+					fmt.Fprintln(w, "Orange team name missing")
+				}
+				if initSession.BlueTeamName == nil {
+					fmt.Fprintln(w, "Blue team name missing")
+				}
+				if initSession.MapPool == nil {
+					fmt.Fprintln(w, "Map pool missing")
+				}
 				return
+			}
+
+			// Check if team names are blank
+			if *initSession.OrangeTeamName == "" || *initSession.BlueTeamName == "" {
+				w.WriteHeader(http.StatusBadRequest)
+				if *initSession.OrangeTeamName == "" {
+					fmt.Fprintln(w, "Orange team name cannot be blank")
+				}
+				if *initSession.OrangeTeamName == "" {
+					fmt.Fprintln(w, "Blue team name cannot be missing")
+				}
+			}
+
+			// Check if submitted map pool are allowed
+			for _, m := range initSession.MapPool {
+				if !mapPool[m] {
+					w.WriteHeader(http.StatusBadRequest)
+					fmt.Fprintf(w, "Map not allowed: %s\n", m)
+					return
+				}
 			}
 
 			// JSON decoded -- t contains TeamNames
 			// Create session
 			hostToken := generateToken()
-			s := Session{HostToken: hostToken, OrangeTeamToken: generateToken(), BlueTeamToken: generateToken(), OrangeTeamName: *t.OrangeTeamName, BlueTeamName: *t.BlueTeamName, MapsChosen: []string{}}
-			sessionMap[hostToken] = s                                     // host
-			sessionMap[s.OrangeTeamToken] = Session{HostToken: hostToken} // orange team
-			sessionMap[s.BlueTeamToken] = Session{HostToken: hostToken}   // blue team
+			s := Session{HostToken: hostToken, OrangeTeamToken: generateToken(), BlueTeamToken: generateToken(), OrangeTeamName: *initSession.OrangeTeamName, BlueTeamName: *initSession.BlueTeamName, MapPool: initSession.MapPool, MapsChosen: make([]string, 3), MapsBanned: make([]string, 3)}
+			sessionMap[hostToken] = s                                                                         // host
+			sessionMap[s.OrangeTeamToken] = Session{HostToken: hostToken, OrangeTeamToken: s.OrangeTeamToken} // orange team
+			sessionMap[s.BlueTeamToken] = Session{HostToken: hostToken, BlueTeamToken: s.BlueTeamToken}       // blue team
 
 			// Encode session as JSON response
 			sessionJson, err := json.Marshal(s)
@@ -162,6 +226,68 @@ func handlerBuilder(sessionMap map[string]Session) func(http.ResponseWriter, *ht
 			w.WriteHeader(http.StatusOK)
 			w.Header().Set("Content-Type", "application/json")
 			w.Write(sessionJson)
+		case "1":
+			// Phase 1 -- Orange Team ban
+			// Expects token from an Orange Team
+			// {
+			// 		token: "...",
+			//		choice: "..."
+			// }
+			var mapChoice MapChoice
+			b := make([]byte, r.ContentLength)
+			r.Body.Read(b)
+			if err := json.Unmarshal(b, &mapChoice); err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Fprintln(w, "Cannot parse JSON request body")
+				return
+			}
+
+			// Check for nil fields
+			if mapChoice.Choice == nil || mapChoice.Token == nil {
+				w.WriteHeader(http.StatusBadRequest)
+				if mapChoice.Choice == nil {
+					fmt.Fprintln(w, "Map choice missing")
+				}
+				if mapChoice.Token == nil {
+					fmt.Fprintln(w, "Token missing")
+				}
+			}
+
+			// Check if token references active session
+			session, found := sessionMap[*mapChoice.Token]
+			if !found {
+				w.WriteHeader(http.StatusInternalServerError) // server fault since sessions must be preserved
+				fmt.Fprintln(w, "Session not found")
+				return
+			}
+
+			// Session found, find host session and
+			// check if it is from an orange team
+			if *mapChoice.Token != session.OrangeTeamToken {
+				w.WriteHeader(http.StatusForbidden) // not orange team
+				fmt.Fprintln(w, "Not orange team")
+				return
+			}
+
+			// Session from orange team confirmed
+			// Check if map banned is in host session's map pool
+			found = false
+			for _, m := range session.MapPool {
+				if m == *mapChoice.Choice {
+					found = true
+					break
+				}
+			}
+			if !found {
+				w.WriteHeader(http.StatusBadRequest)
+				fmt.Fprintf(w, "Map not found in host map pool: %s\n", *mapChoice.Choice)
+				return
+			}
+
+			// All verified. Append to list of banned maps
+			session.MapsBanned = append(session.MapsBanned, *mapChoice.Choice)
+			w.WriteHeader(http.StatusOK)
+			return
 		case "8":
 			// Phase 8 (results phase), mapban done
 			// Close session
@@ -217,4 +343,5 @@ func handlerBuilder(sessionMap map[string]Session) func(http.ResponseWriter, *ht
 			fmt.Fprintln(w, "Will be handled soon")
 		}
 	}
+
 }
--- includes/structs.go
+++ includes/structs.go
@@ -1,9 +1,10 @@
 package includes
 
 // Incoming JSON document. Must be checked for below fields.
-type TeamNames struct {
-	OrangeTeamName *string `json:"orangeTeamName"`
-	BlueTeamName   *string `json:"blueTeamName"`
+type InitSession struct {
+	OrangeTeamName *string  `json:"orangeTeamName"`
+	BlueTeamName   *string  `json:"blueTeamName"`
+	MapPool        []string `json:"mapPool"`
 }
 
 // Session represents a mapban session.
@@ -27,6 +28,20 @@ type Session struct {
 	OrangeTeamName  string `json:"orangeTeamName"`
 	BlueTeamName    string `json:"blueTeamName"`
 
-	// Follows the following format: [OT ban, BT ban, OT pick, BT pick, OT ban, BT ban, decider]
+	// Map pool, all maps chosen for the session
+	MapPool []string `json:"mapPool"`
+
+	// Follows the following format: [OT pick, BT pick, decider]
 	MapsChosen []string `json:"mapsChosen"`
+
+	// Used during pick phases (phases 3, 4, and 7) to prevent being picked
+	MapsBanned []string `json:"mapsBanned"`
+}
+
+type MapChoice struct {
+	// Token
+	Token *string `json:"token"`
+
+	// Map chosen (used for both bans and picks)
+	Choice *string `json:"choice"`
 }
