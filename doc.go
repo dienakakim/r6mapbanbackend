96ad5c45d915f419b0b10f04d218a494d48269f4
Dien Tran <usbv135w@gmail.com>
2020-05-17 04:54:48 -0500

    Removed phase 8 as it is no longer present

--- doc.go
+++ doc.go
@@ -7,11 +7,12 @@
 // Here's the workflow:
 //
 // 1. Phase 0:
-//	  H connects to S, S generates tokens for H, O and B to connect and start session.
+//	  H connects to S, S creates sessions for H, O, and B, represented by Base64URL-encoded tokens.
 // 	  H gives tokens to O and B so they can connect and participate.
 // 2. Phase 1-7:
 //	  Ban/pick phases: O ban, B ban, O pick, B pick, O ban, B ban, H decider.
 //	  In each phase, the respective side sends a request to S initiating each action.
-// 3. Phase 8:
-//	  Results phase, when S frees all sessions. Only the host can access this phase.
+//	  Phase 7 is also the ending phase that terminates the session. After the host sends
+//    the final request, S closes all 3 sessions from H, O, and B, and returns the 3 maps chosen as
+//	  the final output for the mapban session.
 package r6mapbanbackend
